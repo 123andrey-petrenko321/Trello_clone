53cc984481aa3bd40ec752ff0bcba98f45454674
Andrey Petrenko <123petrotreiler321@gmail.com>
2020-01-23 23:16:59 +0200

    d&d card

--- column.js
+++ column.js
@@ -1,5 +1,6 @@
 const Column = {
-    dragged:null,
+    dropped: null,
+    dragged: null,
     idCounter: 4,
     process(columnElement) {
         const spanAction_addNote = columnElement.querySelector('[data-action-addNote]')
@@ -21,46 +22,77 @@ const Column = {
         columnElement.addEventListener('dragstart', Column.dragstart)
         columnElement.addEventListener('dragend', Column.dragend)
 
-        columnElement.addEventListener('dragenter', Column.dragenter)
+        // columnElement.addEventListener('dragenter', Column.dragenter)
         columnElement.addEventListener('dragover', Column.dragover)
-        columnElement.addEventListener('dragleave', Column.dragleave)
-
-        columnElement.addEventListener('dragover', Column.dragover)
+        // columnElement.addEventListener('dragleave', Column.dragleave)
         columnElement.addEventListener('drop', Column.drop)
     },
-    dragstart(event){
+    dragstart(event) {
         Column.dragged = this
         Column.dragged.classList.add('dragged')
         event.stopPropagation();
+
+        document
+            .querySelectorAll('.note')
+            .forEach(noteElement => noteElement.removeAttribute('draggable'))
     },
-    dragend(event){
+    dragend(event) {
         Column.dragged.classList.remove('dragged')
         Column.dragged = null
+        Column.dropped = null
+        document
+            .querySelectorAll('.note')
+            .forEach(noteElement => noteElement.setAttribute('draggable', true))
     },
-    dragenter(event){
-        if(!Column.dragged || Column.dragged === this){
-            return
-        }
-    },
-    dragover(event){
-        event.preventDefault();
-        if(!Column.dragged || Column.dragged === this){
-            return
-        }
-    },
-    dragleave(event){
-        if(!Column.dragged || Column.dragged === this){
-            return
-        }
-    },
+    // dragenter(event) {
+    //     if (!Column.dragged || Column.dragged === this) {
+    //         return
+    //     }
+    //     this.classList.add('under')
+    // },
     dragover(event) {
         event.preventDefault();
+        event.stopPropagation()
+
+        if (Column.dragged === this) {
+            if (Column.dropped) {
+                Column.dropped.classList.remove('under')
+            }
+            Column.dropped = null
+        }
+
+        if (!Column.dragged || Column.dragged === this) {
+            return
+        }
+        Column.dropped = this
+        document
+            .querySelectorAll('.column')
+            .forEach(columnElement => columnElement.classList.remove('under'))
+
+        this.classList.add('under')
     },
+    // dragleave(event) {
+    //     if (!Column.dragged || Column.dragged === this) {
+    //         return
+    //     }
+    //     this.classList.remove('under')
+    // },
     drop(event) {
         if (Note.dragged) {
             return this.querySelector('[data-notes]').append(Note.dragged)
-        }else if( Column.dragged){
-            console.log('drop column')
+        } else if (Column.dragged) {
+            const children = Array.from(document.querySelector('.columns').children)
+            const indexA = children.indexOf(this)
+            const indexB = children.indexOf(Column.dragged)
+
+            if (indexA < indexB) {
+                document.querySelector('.columns').insertBefore(Column.dragged, this)
+            } else {
+                document.querySelector('.columns').insertBefore(Column.dragged, this.nextElementSiblimg)
+            }
+            document
+                .querySelectorAll('.column')
+                .forEach(columnElement => columnElement.classList.remove('under'))
         }
     }
 }
